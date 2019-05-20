80a7d8e5663d5e31421ae7f838d8ec2cf0ae806e
Felix <miaochaofelix@qq.com>
2019-05-20 21:07:45 +0800

    add file demo

--- src/main/java/com/miaochaofelix/tutorial/io/FileDemo.java
+++ src/main/java/com/miaochaofelix/tutorial/io/FileDemo.java
@@ -1,4 +1,23 @@
 package com.miaochaofelix.tutorial.io;
 
+import java.io.File;
+
 public class FileDemo {
+    public static void main(String[] args) {
+        String dir = "/";
+        File file = new File(dir);
+        if (file.isDirectory()) {
+            String subFiles[] = file.list();
+            for (int i = 0; i < subFiles.length; i++) {
+                File subFile = new File( dir + "/" + subFiles[i]);
+                if (subFile.isDirectory()) {
+                    System.out.println(" ./" +subFiles[i] + "/");
+                } else {
+                    System.out.println(" ./" +subFiles[i]);
+                }
+            }
+        } else {
+            System.out.println(file);
+        }
+    }
 }
